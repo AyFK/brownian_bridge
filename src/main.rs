33360a6c1e3b9295a2672e6e6895aca8ab891eb7
David <davidsstudier@gmail.com>
2025-03-08 10:06:17 +0100

    incremental resolution improvements on the original time partition

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use rand::Rng;
 use rand_distr::{Distribution, Normal};
 
 mod plots;
-use plots::resolution::figure;
+#[allow(unused_imports)]
+use plots::resolution::{figure_1, figure_2};
 
 #[allow(dead_code)]
 fn input(prompt: &str) -> String {
@@ -17,12 +18,12 @@ fn input(prompt: &str) -> String {
 }
 
 /// Generate a uniform partition on the time interval
-/// [a, b] with 'n' values, 'n-1' increments.
-fn linspace(a: f64, b: f64, n: usize) -> Vec<f64> {
-    let step_size = (b - a) / (n as f64 - 1.0);
-    let mut values = Vec::with_capacity(n);
+/// [a, b] with 'k' values, 'k-1' increments.
+fn linspace(a: f64, b: f64, k: usize) -> Vec<f64> {
+    let step_size = (b - a) / (k as f64 - 1.0);
+    let mut values = Vec::with_capacity(k);
 
-    for i in 0..n {
+    for i in 0..k {
         values.push(a + (i as f64) * step_size);
     }
 
@@ -64,15 +65,14 @@ fn recursive_fill(time_partition: &[f64], brownian: &mut [f64], start: usize,
     // recursively fill the left and right intervals
     recursive_fill(time_partition, brownian, start, mid, sigma, rng);
     recursive_fill(time_partition, brownian, mid, end, sigma, rng);
-
 }
 
 
-/// Generate a 'n' higher resolution Brownian motion path in
-/// time interval [a, b].
-fn brownian_bridge(n: usize, a: f64, b: f64, sigma: f64, start_val: f64,
+/// Generate a 'k' higher resolution Brownian motion path in
+/// time partition {a, b}.
+fn brownian_bridge(k: usize, a: f64, b: f64, sigma: f64, start_val: f64,
                    end_val: f64, rng: &mut impl Rng) -> (Vec<f64>, Vec<f64>) {
-    let num_points = n + 2;
+    let num_points = k + 2;
     let time_partition = linspace(a, b, num_points);
     let mut brownian = vec![0.0; num_points];
 
@@ -84,25 +84,86 @@ fn brownian_bridge(n: usize, a: f64, b: f64, sigma: f64, start_val: f64,
 }
 
 
-fn main() {
+/// Given a time partition with more than 1 interval: {a, b,
+/// c, ..., n}, generate a 'k' higher resolution Brownian
+/// motion path in-between all n-1 intervals.
+fn refine_all_increments(time: &[f64], w: &[f64], k: usize, sigma: f64,
+                         rng: &mut impl Rng) -> (Vec<f64>, Vec<f64>) {
 
+    let mut new_t = Vec::new();
+    let mut new_w = Vec::new();
+    let num_increments = time.len() - 1;
+
+    // in each interval, build a Brownian bridge
+    for i in 0..num_increments {
+        let (seg_time, seg_w) = brownian_bridge(k, time[i], time[i + 1],
+                                                sigma, w[i], w[i + 1], rng);
+
+        // for the first segment include all points: [t_0, t_i],
+        // for next segments skip the first point i.e: (t_i, t_j]
+        // to avoid duplicating the common endpoint 't_i'
+        if i == 0 {
+            new_t.extend(seg_time);
+            new_w.extend(seg_w);
+        }
+        else {
+            new_t.extend(seg_time.into_iter().skip(1));
+            new_w.extend(seg_w.into_iter().skip(1));
+        }
+    }
+
+    return (new_t, new_w);
+}
+
+
+/// Refines the Brownian motion path using a vector of
+/// incremental resolution improvement values 'k'.
+fn incremental_refinment(resolution_increases: &[usize], a: f64, b: f64,
+                         sigma: f64, start_val: f64, end_val: f64,
+                         rng: &mut impl Rng)
+                         -> Vec<(usize, Vec<f64>, Vec<f64>)> {
+
+    // store resoluts here
+    let mut refinements = Vec::new();
+
+    // start with the original time partition {a, b}
+    let mut current_time = vec![a, b];
+    let mut current_w = vec![start_val, end_val];
+
+    // refine path incrementally
+    for &k in resolution_increases {
+        let (new_time, new_w) = refine_all_increments(&current_time, &current_w, k, sigma, rng);
+        refinements.push((k, new_time.clone(), new_w.clone()));
+
+        // update current path for next refinement
+        current_time = new_time;
+        current_w = new_w;
+    }
+
+    return refinements;
+}
+
+fn main() {
     let mut rng = rng();
 
     let a = 0.0;
-    let b = 1.0;
+    let b = 2.0;
+    let sigma = 150.0;
 
 
-    let sigma = 1.0;
-    let extra_res = 5;
+    //let res_increase = vec![1, 1, 1, 1, 1, 1, 1];
+    let res_increase = vec![3, 3, 3];
 
-    let start_val = 0.0;
+    // sample the starting and ending values
     let normal = Normal::new(0.0, sigma).unwrap();
+    let start_val = normal.sample(&mut rng);
     let end_val = normal.sample(&mut rng);
 
+    // call recursive_bridge to get the refined partitions
+    let results = incremental_refinment(&res_increase, a, b, sigma,
+                                        start_val, end_val, &mut rng);
 
-    let (t, w) = brownian_bridge(extra_res, a, b, sigma, start_val, end_val,
-                                 &mut rng);
+    figure_2(results);
 
-    figure(&[a, b], &[start_val, end_val], &t, &w);
     let _ = input("[ENTER] to exit");
 }
--- src/plots/resolution.rs
+++ src/plots/resolution.rs
@@ -1,9 +1,8 @@
 
-
 use gnuplot::*;
 
-pub fn figure(low_res_time: &[f64], low_res_brownian: &[f64],
-              high_res_time: &[f64], high_res_brownian: &[f64]) {
+pub fn figure_1(low_res_time: &[f64], low_res_brownian: &[f64],
+                high_res_time: &[f64], high_res_brownian: &[f64]) {
 
     let col1 = "#000000";
     let col2 = "#2CA02C";
@@ -22,6 +21,68 @@ pub fn figure(low_res_time: &[f64], low_res_brownian: &[f64],
     ax.points(high_res_time, high_res_brownian, &[Color(col2), PointSymbol('o')]);
 
 
+
+    // set smoother gnuplot terminal and show it
+    fg.set_terminal("wxt", "");
+
+    // spawn plot on new thread to let Rust code continue
+    std::thread::spawn(move || {
+        fg.show().unwrap();
+    });
+}
+
+
+
+enum Color {
+    Black,
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Cyan,
+    Magenta,
+    Silver,
+}
+
+
+impl Color {
+    fn to_hex(&self) -> String {
+        match self {
+            Color::Black => String::from("#000000"),
+            Color::Red => String::from("#FF0000"),
+            Color::Green => String::from("#2CA02C"),
+            Color::Blue => String::from("#0000FF"),
+            Color::Yellow => String::from("#FFD700"),
+            Color::Cyan => String::from("#00FFFF"),
+            Color::Magenta => String::from("#D100D1"),
+            Color::Silver => String::from("#C0C0C0"),
+        }
+    }
+}
+
+
+pub fn figure_2(results: Vec<(usize, Vec<f64>, Vec<f64>)>) {
+
+    let num_plots = results.len();
+    let colors = vec![Color::Black, Color::Red, Color::Green,
+                      Color::Blue, Color::Yellow, Color::Cyan,
+                      Color::Magenta, Color::Silver];
+
+    let mut fg = Figure::new();
+    let ax = fg.axes2d();
+    ax.set_x_grid(true);
+    ax.set_y_grid(true);
+
+    for i in 0..num_plots {
+        let col = &colors[i];
+
+        let (_, time, val) = &results[i];
+
+
+        ax.lines(time, val, &[Color(&col.to_hex())]);
+        ax.points(time, val, &[Color(&col.to_hex()), PointSymbol('o'), PointSize(1.0 + (1.0 / ((i+1) as f64)))]);
+
+    }
 
     // set smoother gnuplot terminal and show it
     fg.set_terminal("wxt", "");
